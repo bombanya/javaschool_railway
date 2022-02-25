7d3080ae0bd6268f7e77279874e26bfc1c23d8d2
bombanya <proshkin20020129@mail.ru>
2022-02-25 16:58:24 +0300

    add region entity

---
+++ src/main/java/com/bombanya/javaschool_railway/entities/geography/Region.java
@@ -0,0 +1,30 @@
+package com.bombanya.javaschool_railway.entities.geography;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "region")
+@Setter
+@Getter
+@ToString
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "region_seq")
+    @SequenceGenerator(name = "region_seq",
+            sequenceName = "region_region_id_seq",
+            allocationSize = 1)
+    @Column(name = "region_id")
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+}
