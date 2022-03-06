0bb5277dfbec5739881c79367bbcb8b3ea801ba8
bombanya <proshkin20020129@mail.ru>
2022-03-06 19:51:01 +0300

    passenger base controller

---
+++ src/main/java/com/bombanya/javaschool_railway/controllers/PassengerController.java
@@ -0,0 +1,33 @@
+package com.bombanya.javaschool_railway.controllers;
+
+import com.bombanya.javaschool_railway.JacksonView;
+import com.bombanya.javaschool_railway.entities.Passenger;
+import com.bombanya.javaschool_railway.entities.ServiceAnswer;
+import com.bombanya.javaschool_railway.services.PassengerService;
+import com.bombanya.javaschool_railway.services.ServiceAnswerHelper;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/passenger")
+@RequiredArgsConstructor
+public class PassengerController {
+
+    private final PassengerService passengerService;
+
+    @PostMapping("/new")
+    @JsonView(JacksonView.UserInfo.class)
+    public ResponseEntity<ServiceAnswer<Passenger>> saveNewPassenger(@RequestBody Passenger passenger){
+        return ServiceAnswerHelper.wrapIntoResponse(passengerService.saveNew(passenger));
+    }
+
+    @GetMapping("/all")
+    @JsonView(JacksonView.UserInfo.class)
+    public ResponseEntity<ServiceAnswer<List<Passenger>>> getAllPassengers(){
+        return ServiceAnswerHelper.wrapIntoResponse(passengerService.getAll());
+    }
+}
---
+++ src/main/java/com/bombanya/javaschool_railway/dao/PassengerDAOImpl.java
@@ -0,0 +1,37 @@
+package com.bombanya.javaschool_railway.dao;
+
+import com.bombanya.javaschool_railway.entities.Passenger;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class PassengerDAOImpl implements PassengerDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    @Transactional(noRollbackFor = PersistenceException.class)
+    public void save(Passenger entity) {
+        em.persist(entity);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Passenger> findById(Integer integer) {
+        if (integer == null) return Optional.empty();
+        return Optional.ofNullable(em.find(Passenger.class, integer));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Passenger> findAll() {
+        return em.createQuery("select p from Passenger p", Passenger.class).getResultList();
+    }
+}
